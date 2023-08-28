27866cc0846405d4067f60e9df592783057ecc9e
Ivan Guimarães Farias <iguimaraesf@gmail.com>
2023-08-28 09:52:01 -0300

    [feat] Aula 01

--- src/rotas/rotas-motorista/rotas-motorista.service.ts
+++ src/rotas/rotas-motorista/rotas-motorista.service.ts
@@ -5,7 +5,42 @@ import { PrismaService } from '../../prisma/prisma/prisma.service'
 export class RotasMotoristaService {
     constructor(private prismaService: PrismaService) {}
 
-    criarOuAtualizar(dto: { id_rota: string; lat: number; lng: number }) {
-        
+    async criarOuAtualizar(dto: { id_rota: string; lat: number; lng: number }) {
+/*
+        this.prismaService.rotaMotorista.count({
+            where: {
+                id_rota: dto.id_rota
+            }
+        })
+*/
+        return await this.prismaService.rotaMotorista.upsert({
+            include: {
+                rota: true,
+            },
+            where: {
+                id_rota: dto.id_rota
+            },
+            create: {
+                id_rota: dto.id_rota,
+                pontos: {
+                    set: {
+                        localidade: {
+                            lat: dto.lat,
+                            lng: dto.lng,
+                        },
+                    },
+                },
+            },
+            update: {
+                pontos: {
+                    set: {
+                        localidade: {
+                            lat: dto.lat,
+                            lng: dto.lng,
+                        },
+                    },
+                },
+            },
+        })
     }
 }
